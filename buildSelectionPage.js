e4b21d662b725181fcc81f216a4ab5963dda060b
Kevin Le <kevinle108@gmail.com>
2021-09-27 13:31:32 -0400

    revert back to older method of br aria-hidden

--- buildSelectionPage.js
+++ buildSelectionPage.js
@@ -203,11 +203,11 @@ function buildRankChoiceRace(race, raceIndex) {
 }
 
 function buildRankHeaders(race) {
-  // const headerHtml = `<th class="cell" aria-label="{RANK} Choice">{RANK}<br aria-hidden="true">Choice</th>`; //scope="col"
-  const headerHtml = `
-  <th class="cell" aria-label="{RANK} Choice">
-      <div>{RANK}<br aria-hidden="true"/>Choice</div>
-  </th>`; //scope="col"
+  const headerHtml = `<th class="cell" aria-label="{RANK} Choice">{RANK}<br aria-hidden="true">Choice</th>`; //scope="col"
+  // const headerHtml = `
+  // <th class="cell" aria-label="{RANK} Choice">
+  //     <div>{RANK}<br aria-hidden="true"/>Choice</div>
+  // </th>`;
 
   let html = '';
   let rank = 1;
